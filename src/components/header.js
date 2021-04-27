a47171b64dbb00391e7048f84cbdef2211759ec9
qiaod <qiaod@aquait.co.jp>
2021-04-27 16:49:15 +0900

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,34 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from '../images/logo.png';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <nav className="navbar is-transparent" 
+    style={{ maxWidth: '960px', margin: '10px auto'}}>
+    <div className="container">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+          <img src={logo} alt="aqua" style={{ width: '88px', margin: '0' }} />         
         </Link>
-      </h1>
+      </div>
+    
+      <div id="navMenu" className="navbar-end has-text-centered" >
+        <Link className="navbar-item" to="/about">
+          About us
+        </Link>
+        <Link className="navbar-item" to="/solutions">
+          Solutions
+        </Link>
+        <Link className="navbar-item" to="/case">
+          Case study
+        </Link>
+        <Link className="navbar-item" to="/contact">
+          Contact
+        </Link>
+      </div>
     </div>
-  </header>
+  </nav>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,16 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+
+    {/* <StaticImage
+      src="../images/background.jpg"
+      width= {500}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    /> */}
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -20,7 +30,6 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
